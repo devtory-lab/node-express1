26f70f5d3efc8083fb2aded3b98cda204006c51c
devtory-lab <an44andedu@gmail.com>
2022-06-15 15:21:04 +0900

    회원가입 페이지 구성

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -6,7 +6,10 @@ const router = express.Router();
 const ctrl = require("./home.ctrl");
 
 router.get("/", ctrl.output.home);		//ctrl.output.hello 이 더 좋다.
+
 router.get("/login", ctrl.output.login);
 router.post("/login", ctrl.process.login);
+
+router.get("/register", ctrl.output.register);
 
 module.exports = router;
